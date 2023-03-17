20fccfe0e87ed37237cc0971ac9da56ec1d46e8f
adowls <liqiu2000@gmail.com>
2023-03-17 21:16:31 +0800

    rmove qosify

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -8,6 +8,7 @@ git checkout target/linux/rockchip/Makefile
 rm target/linux/rockchip/patches-5.10/002-net-usb-r8152-add-LED-configuration-from-OF.patch
 rm target/linux/rockchip/patches-5.10/003-dt-bindings-net-add-RTL8152-binding-documentation.patch
 rm target/linux/rockchip/patches-5.10/600-net-phy-Add-driver-for-Motorcomm-YT85xx-PHYs.patch
+mkdir target/linux/rockchip/files/include/dt-bindings/soc/
 cp ../etc/rockchip-vop2.h target/linux/rockchip/files/include/dt-bindings/soc/rockchip-vop2.h
 
 ./scripts/feeds update -a
@@ -19,6 +20,7 @@ sed -i '/.*rockpro64/d' .config
 sed -i '/.*rock-pi-4a/d' .config
 sed -i 's/r4s/r4se/' .config
 sed -i 's/CONFIG_TARGET_ALL_PROFILES=y/# CONFIG_TARGET_ALL_PROFILES is not set/' .config
+sed -i 's/CONFIG_PACKAGE_qosify=m/# CONFIG_PACKAGE_qosify is not set/' .config
 make defconfig
 
 make -j11 download

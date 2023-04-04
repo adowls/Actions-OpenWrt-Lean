9e24eb415021fed652a38f72e5e30e425547706a
adowls <liqiu2000@gmail.com>
2023-04-04 13:18:58 +0800

    kernel 5.10.161 worked

--- scripts/5.10.161.prepare.openwrt.sh
+++ scripts/5.10.161.prepare.openwrt.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-# rm .gitignore
-# git clean -df
-# rm -Rf feeds
-# git checkout .
-# git checkout v22.03.3
-make dirclean
+rm .gitignore
+git clean -df
+git checkout .
+make distclean
+# make target/linux/clean
 
 # cp -R ../lede/config .
 # cp -R ../lede/target/linux/generic/config-5.15 target/linux/generic/
@@ -19,7 +18,7 @@ cp -R ../lede/target/linux/rockchip target/linux/
 # # # fix openwrt vermagic
 git checkout target/linux/rockchip/armv8/config-5.10
 # # # fix CONFIG_HW_RANDOM_ROCKCHIP waiting choice
-echo 'CONFIG_HW_RANDOM_ROCKCHIP=y' >> target/linux/rockchip/armv8/config-5.10
+echo 'CONFIG_HW_RANDOM_ROCKCHIP=n' >> target/linux/rockchip/armv8/config-5.10
 # sed -i 's/kmod-r8168 -urngd/kmod-r8169/' target/linux/rockchip/image/armv8.mk
 git checkout target/linux/rockchip/Makefile
 rm target/linux/rockchip/patches-5.10/002-net-usb-r8152-add-LED-configuration-from-OF.patch
@@ -73,7 +72,7 @@ cp ../lede/target/linux/rockchip/patches-5.15/105-rockchip-rock-pi-4.patch targe
 # echo "LINUX_KERNEL_HASH-5.15.98 = 7dc62cd3a45f95c9b60316a5886ea9406aee256308869dac1e4ec088fbb37787" >> include/kernel-5.15
 
 # sed -i 's/$/&;master/' feeds.conf.default
-# ./scripts/feeds update -a && ./scripts/feeds install -a
+./scripts/feeds update -a && ./scripts/feeds install -a
 
 wget https://downloads.openwrt.org/releases/22.03.3/targets/rockchip/armv8/config.buildinfo -O .config
 sed -i '/.*r2s/d' .config
@@ -82,7 +81,10 @@ sed -i '/.*rock-pi-4a/d' .config
 sed -i 's/r4s/r4se/' .config
 sed -i 's/CONFIG_TARGET_MULTI_PROFILE=y/# CONFIG_TARGET_MULTI_PROFILE is not set/' .config
 sed -i 's/CONFIG_TARGET_ALL_PROFILES=y/# CONFIG_TARGET_ALL_PROFILES is not set/' .config
+sed -i 's/CONFIG_TARGET_PER_DEVICE_ROOTFS=y/# CONFIG_TARGET_PER_DEVICE_ROOTFS is not set/' .config
 sed -i 's/CONFIG_TARGET_DEVICE_PACKAGES_rockchip_armv8_DEVICE_friendlyarm_nanopi-r4se=""/CONFIG_TARGET_rockchip_armv8_DEVICE_friendlyarm_nanopi-r4se=y/' .config
+echo 'CONFIG_TARGET_ROOTFS_SQUASHFS=y' >> .config
+echo '# CONFIG_TARGET_ROOTFS_EXT4FS is not set' >> .config
 
 # echo 'CONFIG_TARGET_rockchip=y' > .config
 # echo 'CONFIG_TARGET_rockchip_armv8=y' >> .config
@@ -138,14 +140,59 @@ sed -i 's/CONFIG_TARGET_DEVICE_PACKAGES_rockchip_armv8_DEVICE_friendlyarm_nanopi
 # echo 'CONFIG_PACKAGE_rpcd-mod-rrdns=y' >> .config
 # echo 'CONFIG_PACKAGE_uhttpd=y' >> .config
 # echo 'CONFIG_PACKAGE_uhttpd-mod-ubus=y' >> .config
+
+# sed -i '/CONFIG_ALL_KMODS=y/d' .config
+# sed -i '/CONFIG_ALL_NONSHARED=y/d' .config
+# sed -i '/CONFIG_DEVEL=y/d' .config
+# # # sed -i '/CONFIG_AUTOREMOVE=y/d' .config
+# sed -i '/CONFIG_BPF_TOOLCHAIN_BUILD_LLVM=y/d' .config
+# # sed -i '/# CONFIG_BPF_TOOLCHAIN_NONE is not set/d' .config
+# sed -i '/CONFIG_BUILDBOT=y/d' .config
+# # # sed -i '/CONFIG_COLLECT_KERNEL_DEBUG=y/d' .config
+# sed -i '/CONFIG_HAS_BPF_TOOLCHAIN=y/d' .config
+# sed -i '/CONFIG_IB=y/d' .config
+# sed -i '/CONFIG_IMAGEOPT=y/d' .config
+# # # sed -i '/CONFIG_KERNEL_BUILD_DOMAIN=\"buildhost\"/d' .config
+# # # sed -i '/CONFIG_KERNEL_BUILD_USER=\"builder\"/d' .config
+# # # sed -i '/# CONFIG_KERNEL_KALLSYMS is not set/d' .config
+# # # sed -i '/CONFIG_MAKE_TOOLCHAIN=y/d' .config
+# # sed -i '/CONFIG_PACKAGE_libbpf=m/d' .config
+# # sed -i '/CONFIG_PACKAGE_libelf=m/d' .config
+# # sed -i '/CONFIG_PACKAGE_luci-ssl=y/d' .config
+# # sed -i '/CONFIG_PACKAGE_px5g-wolfssl=y/d' .config
+# # sed -i '/CONFIG_PACKAGE_qosify=m/d' .config
+# # sed -i '/CONFIG_PACKAGE_tc-full=m/d' .config
+# # sed -i '/CONFIG_PACKAGE_tc-mod-iptables=m/d' .config
+# # sed -i '/CONFIG_PACKAGE_zlib=m/d' .config
+# # # sed -i '/CONFIG_REPRODUCIBLE_DEBUG_INFO=y/d' .config
+# # # sed -i '/CONFIG_SDK=y/d' .config
+# # # sed -i '/CONFIG_SDK_LLVM_BPF=y/d' .config
+# # # sed -i '/CONFIG_USE_LLVM_BUILD=y/d' .config
+# # # sed -i '/CONFIG_VERSIONOPT=y/d' .config
+# # # sed -i '/CONFIG_VERSION_BUG_URL=\"\"/d' .config
+# # # sed -i '/CONFIG_VERSION_CODE=\"\"/d' .config
+# # # sed -i '/CONFIG_VERSION_DIST=\"OpenWrt\"/d' .config
+# # # sed -i '/CONFIG_VERSION_FILENAMES=y/d' .config
+# # # sed -i '/CONFIG_VERSION_HOME_URL=\"\"/d' .config
+# # # sed -i '/CONFIG_VERSION_HWREV=\"\"/d' .config
+# # # sed -i '/CONFIG_VERSION_MANUFACTURER=\"\"/d' .config
+# # # sed -i '/CONFIG_VERSION_MANUFACTURER_URL=\"\"/d' .config
+# # # sed -i '/CONFIG_VERSION_NUMBER=\"\"/d' .config
+# # # sed -i '/CONFIG_VERSION_PRODUCT=\"\"/d' .config
+# # # sed -i '/CONFIG_VERSION_REPO=\"https:\/\/downloads.openwrt.org\/releases\/22.03.3\"/d' .config
+# # # sed -i '/CONFIG_VERSION_SUPPORT_URL=\"\"/d' .config
+
 make defconfig
+sed -i 's/CONFIG_PACKAGE_r8169-firmware=m/CONFIG_PACKAGE_r8169-firmware=y/' .config
+sed -i 's/CONFIG_PACKAGE_kmod-r8169=m/CONFIG_PACKAGE_kmod-r8169=y/' .config
+sed -i 's/CONFIG_PACKAGE_kmod-mii=m/CONFIG_PACKAGE_kmod-mii=y/' .config
+sed -i 's/CONFIG_PACKAGE_kmod-phy-realtek=m/CONFIG_PACKAGE_kmod-phy-realtek=y/' .config
+sed -i 's/CONFIG_PACKAGE_kmod-mdio-devres=m/CONFIG_PACKAGE_kmod-mdio-devres=y/' .config
+sed -i 's/CONFIG_PACKAGE_kmod-libphy=m/CONFIG_PACKAGE_kmod-libphy=y/' .config
+sed -i 's/CONFIG_PACKAGE_kmod-of-mdio=m/CONFIG_PACKAGE_kmod-of-mdio=y/' .config
+sed -i 's/CONFIG_PACKAGE_kmod-fixed-phy=m/CONFIG_PACKAGE_kmod-fixed-phy=y/' .config
+
 # sed -i '/=m$/d' .config
-sed -i '/CONFIG_PACKAGE_zlib=m/d' .config
-sed -i '/CONFIG_REPRODUCIBLE_DEBUG_INFO=y/d' .config
-sed -i '/CONFIG_SDK=y/d' .config
-sed -i '/CONFIG_SDK_LLVM_BPF=y/d' .config
-sed -i '/CONFIG_USE_LLVM_BUILD=y/d' .config
-
-
-# make -j$(nproc) download
-# IGNORE_ERRORS=1 make -j$(nproc) V=sc
+make -j$(nproc) download
+IGNORE_ERRORS=1 make -j$(nproc) V=sc
+# make target/linux/compile -j$(nproc) V=sc
